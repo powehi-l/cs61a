e02ed947b311f8aea416b2e75c43dd1cd84596d0
G <gyj_ccih@126.com>
2022-01-30 21:21:01 +0800

    all

---
+++ lab/lab02/lab02.py
@@ -0,0 +1,19 @@
+"""Lab 2: Lambda Expressions and Higher Order Functions"""
+
+# Lambda Functions
+
+def lambda_curry2(func):
+    """
+    Returns a Curried version of a two-argument function FUNC.
+    >>> from operator import add
+    >>> curried_add = lambda_curry2(add)
+    >>> add_three = curried_add(3)
+    >>> add_three(5)
+    8
+    """
+    "*** YOUR CODE HERE ***"
+    def carry2(x):
+        def carry1(y):
+                return func(x,y)
+        return carry1
+    return carry2
---
+++ note/Lectrue2.py
@@ -0,0 +1,23 @@
+''' 
+    import: from * import *
+    name: any valid name
+    assignment : name = value
+    user-defined functions:'def' 
+'''
+
+from math import pi
+from math import sin
+a = sin(pi/2)
+print(a)
+
+from operator import add, mul
+def square(x):
+    return mul(x,x) 
+
+r = 3
+area = pi * pi * r
+print(square(r))
+
+
+
+
