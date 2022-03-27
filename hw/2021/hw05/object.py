55487aebc329b6f0855e285f3de8ac65080f8229
powehi-l <82803905+powehi-l@users.noreply.github.com>
2022-03-27 19:22:58 +0800

    finish
    finish

---
+++ hw/2021/hw05/object.py
@@ -0,0 +1,10 @@
+# sequence objects
+chinese = ['coin', 'string', 'myriad']  # create a literal
+suits = chinese
+
+suits.pop()
+suits.remove('string')
+suits.append('cup')
+suits.extend(['sword', 'club'])
+suits[2] = 'spade'
+
--- lab/lab05/lab05.py
+++ lab/lab05/lab05.py
@@ -1,111 +1,354 @@
-""" Lab 05: Mutable Sequences and Trees """
-# Q2
-def map(fn, seq):
-    """Applies fn onto each element in seq and returns a list.
+from math import sqrt
 
-    >>> map(lambda x: x*x, [1, 2, 3])
-    [1, 4, 9]
+
+def factors_list(n):
+    """Return a list containing all the numbers that divide `n` evenly, except
+    for the number itself. Make sure the list is in ascending order.
+
+    >>> factors_list(6)
+    [1, 2, 3]
+    >>> factors_list(8)
+    [1, 2, 4]
+    >>> factors_list(28)
+    [1, 2, 4, 7, 14]
+    """
+    all_factors = []
+    "*** YOUR CODE HERE ***"
+    for i in range(1, n):
+        if(n % i == 0):
+            all_factors += [i]
+    return all_factors
+
+
+def flatten(s):
+    """Returns a flattened version of list s.
+
+    >>> flatten([1, 2, 3])     # normal list
+    [1, 2, 3]
+    >>> x = [1, [2, 3], 4]     # deep list
+    >>> flatten(x)
+    [1, 2, 3, 4]
+    >>> x # Ensure x is not mutated
+    [1, [2, 3], 4]
+    >>> x = [[1, [1, 1]], 1, [1, 1]] # deep list
+    >>> flatten(x)
+    [1, 1, 1, 1, 1, 1]
+    >>> x
+    [[1, [1, 1]], 1, [1, 1]]
     """
     "*** YOUR CODE HERE ***"
+    result_list = []
+    for i in s:
+        if(type(i) == list):
+            result_list += flatten(i)
+        else:
+            result_list += [i]
+    return result_list
 
-def filter(pred, seq):
-    """Keeps elements in seq only if they satisfy pred.
 
-    >>> filter(lambda x: x % 2 == 0, [1, 2, 3, 4])
-    [2, 4]
+def distance(city_a, city_b):
+    """
+    >>> city_a = make_city('city_a', 0, 1)
+    >>> city_b = make_city('city_b', 0, 2)
+    >>> distance(city_a, city_b)
+    1.0
+    >>> city_c = make_city('city_c', 6.5, 12)
+    >>> city_d = make_city('city_d', 2.5, 15)
+    >>> distance(city_c, city_d)
+    5.0
     """
     "*** YOUR CODE HERE ***"
+    x1, x2 = get_lat(city_a), get_lat(city_b)
+    y1, y2 = get_lon(city_a), get_lon(city_b)
+    return sqrt((x1-x2)**2+(y1-y2)**2)
 
-def reduce(combiner, seq):
-    """Combines elements in seq using combiner.
 
-    >>> reduce(lambda x, y: x + y, [1, 2, 3, 4])
-    10
-    >>> reduce(lambda x, y: x * y, [1, 2, 3, 4])
-    24
-    >>> reduce(lambda x, y: x * y, [4])
-    4
+def closer_city(lat, lon, city_a, city_b):
+    """
+    Returns the name of either city_a or city_b, whichever is closest to
+    coordinate (lat, lon). If the two cities are the same distance away
+    from the coordinate, consider city_b to be the closer city.
+
+    >>> berkeley = make_city('Berkeley', 37.87, 112.26)
+    >>> stanford = make_city('Stanford', 34.05, 118.25)
+    >>> closer_city(38.33, 121.44, berkeley, stanford)
+    'Stanford'
+    >>> bucharest = make_city('Bucharest', 44.43, 26.10)
+    >>> vienna = make_city('Vienna', 48.20, 16.37)
+    >>> closer_city(41.29, 174.78, bucharest, vienna)
+    'Bucharest'
     """
     "*** YOUR CODE HERE ***"
+    temp = make_city('x', lat, lon)
+    if(distance(temp, city_a) < distance(temp, city_b)):
+        return get_name(city_a)
+    else:
+        return get_name(city_b)
 
-# Q3
-def acorn_finder(t):
-    """Returns True if t contains a node with the value 'acorn' and
+
+def check_city_abstraction():
+    """
+    There's nothing for you to do for this function, it's just here for the extra doctest
+    >>> change_abstraction(True)
+    >>> city_a = make_city('city_a', 0, 1)
+    >>> city_b = make_city('city_b', 0, 2)
+    >>> distance(city_a, city_b)
+    1.0
+    >>> city_c = make_city('city_c', 6.5, 12)
+    >>> city_d = make_city('city_d', 2.5, 15)
+    >>> distance(city_c, city_d)
+    5.0
+    >>> berkeley = make_city('Berkeley', 37.87, 112.26)
+    >>> stanford = make_city('Stanford', 34.05, 118.25)
+    >>> closer_city(38.33, 121.44, berkeley, stanford)
+    'Stanford'
+    >>> bucharest = make_city('Bucharest', 44.43, 26.10)
+    >>> vienna = make_city('Vienna', 48.20, 16.37)
+    >>> closer_city(41.29, 174.78, bucharest, vienna)
+    'Bucharest'
+    >>> change_abstraction(False)
+    """
+
+# Treat all the following code as being behind an abstraction layer,
+# you shouldn't need to look at it.
+
+
+def make_city(name, lat, lon):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_name(city)
+    'Berkeley'
+    >>> get_lat(city)
+    0
+    >>> get_lon(city)
+    1
+    """
+    if change_abstraction.changed:
+        return {"name": name, "lat": lat, "lon": lon}
+    else:
+        return [name, lat, lon]
+
+
+def get_name(city):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_name(city)
+    'Berkeley'
+    """
+    if change_abstraction.changed:
+        return city["name"]
+    else:
+        return city[0]
+
+
+def get_lat(city):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_lat(city)
+    0
+    """
+    if change_abstraction.changed:
+        return city["lat"]
+    else:
+        return city[1]
+
+
+def get_lon(city):
+    """
+    >>> city = make_city('Berkeley', 0, 1)
+    >>> get_lon(city)
+    1
+    """
+    if change_abstraction.changed:
+        return city["lon"]
+    else:
+        return city[2]
+
+
+def berry_finder(t):
+    """Returns True if t contains a node with the value 'berry' and 
     False otherwise.
 
-    >>> scrat = tree('acorn')
-    >>> acorn_finder(scrat)
+    >>> scrat = tree('berry')
+    >>> berry_finder(scrat)
     True
-    >>> sproul = tree('roots', [tree('branch1', [tree('leaf'), tree('acorn')]), tree('branch2')])
-    >>> acorn_finder(sproul)
+    >>> sproul = tree('roots', [tree('branch1', [tree('leaf'), tree('berry')]), tree('branch2')])
+    >>> berry_finder(sproul)
     True
     >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
-    >>> acorn_finder(numbers)
+    >>> berry_finder(numbers)
     False
-    """
-    "*** YOUR CODE HERE ***"
-
-# Q4
-def replace_leaf(t, old, new):
-    """Returns a new tree where every leaf value equal to old has
-    been replaced with new.
-
-    >>> yggdrasil = tree('odin',
-    ...                  [tree('balder',
-    ...                        [tree('thor'),
-    ...                         tree('loki')]),
-    ...                   tree('frigg',
-    ...                        [tree('thor')]),
-    ...                   tree('thor',
-    ...                        [tree('sif'),
-    ...                         tree('thor')]),
-    ...                   tree('thor')])
-    >>> laerad = copy_tree(yggdrasil) # copy yggdrasil for testing purposes
-    >>> print_tree(replace_leaf(yggdrasil, 'thor', 'freya'))
-    odin
-      balder
-        freya
-        loki
-      frigg
-        freya
-      thor
-        sif
-        freya
-      freya
-    >>> laerad == yggdrasil # Make sure original tree is unmodified
+    >>> t = tree(1, [tree('berry',[tree('not berry')])])
+    >>> berry_finder(t)
     True
     """
     "*** YOUR CODE HERE ***"
+    for i in branches(t):
+        if(berry_finder(i) == True):
+            return True
+    if(label(t) == 'berry'):
+        return True
+    else:
+        return False
+
+
+def sprout_leaves(t, leaves):
+    """Sprout new leaves containing the data in leaves at each leaf in
+    the original tree t and return the resulting tree.
+
+    >>> t1 = tree(1, [tree(2), tree(3)])
+    >>> print_tree(t1)
+    1
+      2
+      3
+    >>> new1 = sprout_leaves(t1, [4, 5])
+    >>> print_tree(new1)
+    1
+      2
+        4
+        5
+      3
+        4
+        5
+
+    >>> t2 = tree(1, [tree(2, [tree(3)])])
+    >>> print_tree(t2)
+    1
+      2
+        3
+    >>> new2 = sprout_leaves(t2, [6, 1, 2])
+    >>> print_tree(new2)
+    1
+      2
+        3
+          6
+          1
+          2
+    """
+    "*** YOUR CODE HERE ***"
+    if(is_leaf(t)):
+        return tree(label(t), [tree(i) for i in leaves])
+    else:
+        return tree(label(t), [sprout_leaves(i, leaves) for i in branches(t)])
+
+# Abstraction tests for sprout_leaves and berry_finder
+
+
+def check_abstraction():
+    """
+    There's nothing for you to do for this function, it's just here for the extra doctest
+    >>> change_abstraction(True)
+    >>> scrat = tree('berry')
+    >>> berry_finder(scrat)
+    True
+    >>> sproul = tree('roots', [tree('branch1', [tree('leaf'), tree('berry')]), tree('branch2')])
+    >>> berry_finder(sproul)
+    True
+    >>> numbers = tree(1, [tree(2), tree(3, [tree(4), tree(5)]), tree(6, [tree(7)])])
+    >>> berry_finder(numbers)
+    False
+    >>> t = tree(1, [tree('berry',[tree('not berry')])])
+    >>> berry_finder(t)
+    True
+    >>> t1 = tree(1, [tree(2), tree(3)])
+    >>> print_tree(t1)
+    1
+      2
+      3
+    >>> new1 = sprout_leaves(t1, [4, 5])
+    >>> print_tree(new1)
+    1
+      2
+        4
+        5
+      3
+        4
+        5
+
+    >>> t2 = tree(1, [tree(2, [tree(3)])])
+    >>> print_tree(t2)
+    1
+      2
+        3
+    >>> new2 = sprout_leaves(t2, [6, 1, 2])
+    >>> print_tree(new2)
+    1
+      2
+        3
+          6
+          1
+          2
+    >>> change_abstraction(False)
+    """
+
+
+def change_abstraction(change):
+    """
+    For testing purposes.
+    >>> change_abstraction(True)
+    >>> change_abstraction.changed
+    True
+    """
+    change_abstraction.changed = change
+
+
+change_abstraction.changed = False
+
 
 # Tree ADT
+
 def tree(label, branches=[]):
     """Construct a tree with the given label value and a list of branches."""
-    for branch in branches:
-        assert is_tree(branch), 'branches must be trees'
-    return [label] + list(branches)
+    if change_abstraction.changed:
+        for branch in branches:
+            assert is_tree(branch), 'branches must be trees'
+        return {'label': label, 'branches': list(branches)}
+    else:
+        for branch in branches:
+            assert is_tree(branch), 'branches must be trees'
+        return [label] + list(branches)
+
 
 def label(tree):
     """Return the label value of a tree."""
-    return tree[0]
+    if change_abstraction.changed:
+        return tree['label']
+    else:
+        return tree[0]
+
 
 def branches(tree):
     """Return the list of branches of the given tree."""
-    return tree[1:]
+    if change_abstraction.changed:
+        return tree['branches']
+    else:
+        return tree[1:]
+
 
 def is_tree(tree):
     """Returns True if the given tree is a tree, and False otherwise."""
-    if type(tree) != list or len(tree) < 1:
-        return False
-    for branch in branches(tree):
-        if not is_tree(branch):
+    if change_abstraction.changed:
+        if type(tree) != dict or len(tree) != 2:
             return False
-    return True
+        for branch in branches(tree):
+            if not is_tree(branch):
+                return False
+        return True
+    else:
+        if type(tree) != list or len(tree) < 1:
+            return False
+        for branch in branches(tree):
+            if not is_tree(branch):
+                return False
+        return True
+
 
 def is_leaf(tree):
     """Returns True if the given tree's list of branches is empty, and False
     otherwise.
     """
     return not branches(tree)
+
 
 def print_tree(t, indent=0):
     """Print a representation of this tree in which each node is
@@ -129,6 +372,7 @@ def print_tree(t, indent=0):
     print('  ' * indent + str(label(t)))
     for b in branches(t):
         print_tree(b, indent + 1)
+
 
 def copy_tree(t):
     """Returns a copy of t. Only for testing purposes.
